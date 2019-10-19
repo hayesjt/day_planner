9c75892a46530224a65c703ec7e1e6443ccb0431
haylee tk <haylee430@gmail.com>
2019-10-19 14:20:50 -0500

    javascript done

--- script.js
+++ script.js
@@ -6,39 +6,49 @@ var saveBtn = $(".save-btn");
 var dateSpc = $(".date-box")
 var textBox = $(".discription")
 
+for (var i = 9; i < 18; i++) {
+  if (localStorage.getItem("hour-" + i)) {
+    $("#" + i).val(localStorage.getItem("hour-" + i))
+  }
+}
 
 //DISPLAY DATE ON RIGHT TOP CORNER//
-    //document.getElementById("real-time").innerHTML = dateTime.toDateString();
+//document.getElementById("real-time").innerHTML = dateTime.toDateString();
 $("#real-time").text(moment().format("dddd" + " - " + "MMM Do YY"))
 
 //SAVE TEXT IN FORM TO LOCAL STORAGE//
-saveBtn.on("click", function() {
-      //console.log("clicked!");
+saveBtn.on("click", function () {
+  //console.log("clicked!");
   var value = $(this).siblings(textBox).val();
   var time = $(this).parent().attr("id");
-      //console.log(time);
-     //console.log(value);
+  //console.log(time);
+  //console.log(value);
   localStorage.setItem(time, value);
 });
 
 //CLASS CHANGES BASED ON TIME//
 function hourUpdater() {
   var currentHour = moment().hours();
-    //console.log(currentHour);
-  $(".time-block").each(function(){
-      //var blockHour = parseInt($(this).attr("id").split("-")[1])\
+  //console.log(currentHour);
+  $(".time-block").each(function () {
+    //var blockHour = parseInt($(this).attr("id").split("-")[1])\
     var blockHour = parseInt($(this).attr("id"));
-      //console.log(blockHour);
-    if (blockHour < currentHour){
-      textBox.addClass("past-time");
+    console.log(blockHour);
+    console.log(currentHour);
+    //console.log(blockHour);
+    if (blockHour < currentHour) {
+      $(this).addClass("past-time");
+      console.log("got if")
     }
-    
-    else if (blockHour > currentHour){
-      textBox.addClass("future-time");
+
+    else if (blockHour > currentHour) {
+      $(this).addClass("future-time");
+      console.log("got else if")
     }
 
     else {
-      textBox.addClass("now-time");
+      $(this).addClass("now-time");
+      console.log("got else")
     }
   })
 }
